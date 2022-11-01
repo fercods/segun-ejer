43dd27b176194172f8a40053054d68e8d17125e1
fercods <fernandado@yahoo.com>
2022-11-01 12:11:27 -0300

    primer commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const lista = ["fernando",51,true,("1971/08/06")]
+
+    const libro = {
+        titulo:"El Poder De Las Palabras",
+        autor:" Sigman Mariano",
+        fecha:new Date ("1971/08/06"),
+        url:new URL ("https://www.cuspide.com/9789877950465/El+Poder+De+Las+Palabras")
+        }
+        
+    console.log(lista)
+    console.log(libro)
